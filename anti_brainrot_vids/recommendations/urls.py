7a8cf5d7d0600c82e250b8e0568221aad037c5c9
Giga Hidjrika Aura Adkhy <52191436+gigahidjrikaaa@users.noreply.github.com>
2024-06-08 06:12:31 +0700

    feat: deleted the django project.

--- anti_brainrot_vids/recommendations/urls.py
+++
@@ -1,9 +0,0 @@
-# recommendations/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('recommend/', views.recommend_videos, name='recommend_videos'),
-    path('', views.index, name='index'),
-    path('accounts/login/', views.login_view, name='login'),
-]
